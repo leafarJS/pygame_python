e6807f6e037109793134ad8813431361a6357b92
unknown <riskeconomics@gmail.com>
2022-10-16 10:45:24 -0400

    commit chaning the title logo and background color

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import pygame
 pygame.init()
 
 #create scream 
-scream = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800,600))
+
+#Changing the Title, Logo and Background Color
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('./img/logo.png')
+pygame.display.set_icon(icon)
 
 #game loop 
 running = True
@@ -11,3 +16,6 @@ while running:
   for i in pygame.event.get():
     if i.type == pygame.QUIT:
       running = False
+      
+  screen.fill((255,0,0))
+  pygame.display.update()
