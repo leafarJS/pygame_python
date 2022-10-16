137446dcc30496ce652ba18b3b7b32720df99b38
unknown <riskeconomics@gmail.com>
2022-10-16 11:40:38 -0400

    commit keystroke pressed left right

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ pg.display.set_icon(icon)
 #player
 player_img  = pg.image.load('./img/player.png')
 
-player_x = 370
-player_y = 500
+player_x:float = 370.0
+player_y:float = 500.0
+
+player_x_change:float = 0.0
 
 def player(x, y):
   #Adding Images into Our Space Invader Game
@@ -28,12 +30,26 @@ while running:
   
   screen.fill((255,150,0))
   
-  #Movement Mechanics in Game Development
-  player_y -= 0.1
-  
   for i in pg.event.get():
     if i.type == pg.QUIT:
       running = False
+    
+  #Movement Mechanics in Game Development 
+  # if keystroke is pressed check whether its right or left
+    if i.type == pg.KEYDOWN:
+      print("anything keystroke")
+      if i.key == pg.K_LEFT:
+        print('arrow left is pressed')
+        player_x_change = -0.3
+      if i.key == pg.K_RIGHT:
+        print('arrow right is pressed')
+        player_x_change = 0.3
+    if i.type == pg.KEYUP:
+      if i.key == pg.K_LEFT or i.key == pg.K_RIGHT:
+        print("keystroke has benn released")
+        player_x_change = 0.0
+  
+  player_x += player_x_change
       
   player(player_x, player_y)
   pg.display.update()
